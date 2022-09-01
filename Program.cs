fb396bb11e5964a18d1b34e7fef38fa809fe100e
Yidan Wang <97656451+wyd333@users.noreply.github.com>
2022-09-01 18:19:53 +0800

    C#入门第一次练习

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ret = Tools.Calculator.Mul(10, 20);
+            Console.WriteLine(ret);
+        }
+    }
+}
